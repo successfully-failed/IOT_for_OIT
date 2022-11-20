29c862dfe926f0fa855e09e16fbc233f6a4dc23e
successfully-failed <bronislawwroblewski05@gmail.com>
2022-11-20 02:05:47 +0100

    test_vid.mp4 	video_converter.py

--- sockets.py
+++ sockets.py
@@ -1,19 +1,22 @@
 import websockets
 import asyncio
 import numpy as np
-import pickle
+import base64
+from video_converter import Encoder as En
+
 
 class Socket_manager ():
     def __init__ (self):
-        self.matrix = np.array([[1,1,1],[2,2,2],[3,3,3]])  #video matrix -- 1
+        self.vid = "" 
         self.rules = "" #here comes json --2 
         self.status = "" #for status light --3
         self.cam_id = "" #id of the camera --4 
+        self.logs = ""  # --5
         self.__portname = 8001
         print("run printhelp to know what to do")
 
     def __del__ (self):
-        del self.matrix
+        del self.vid
         del self.rules
         del self.status
         del self.cam_id
@@ -45,8 +48,7 @@ class Socket_manager ():
     async def handler (self, websocket):
         while True:
             self.rules = await websocket.recv()
-            picl = pickle.dumps(self.matrix)
-            await websocket.send(picl)
+            await websocket.send(self.vid)
             await websocket.send(self.rules)
             await websocket.send(self.status)
             await websocket.send(self.cam_id)
@@ -62,4 +64,5 @@ class Socket_manager ():
     def printhelp(self):
         print("After creating an object run main method with assync.runner.run()")
    
-
+soc = Socket_manager()
+asyncio.run(soc.main())
