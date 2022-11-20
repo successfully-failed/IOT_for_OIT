c04c2e058d396abbf940e99a03e97c4304d26d8d
AdemaroDante <jozek.kasprzycki@gmail.com>
2022-11-20 09:42:04 +0100

    Set up getting data by client

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style/Home.css';
 
 
 function Home() {
+    const [cameras, setCameras] = useState([]);
+    const fetch_cam = () => {
+        useEffect(() => {
+                fetch('/cameras')
+                .then(response => response.json())
+                .then(json => setCameras(json))
+        }, []);
+    }
     
-    const cameras = [
-        {id: 0, img: "", status: '2'},
-        {id: 1, img: "", status: '0'},
-        {id: 2, img: "", status: '1'},
-        {id: 3, img: "", status: '0'},
-        {id: 4, img: "", status: '2'},
-    ]
     const listCameras = cameras.map((camera) =>
         <div className='cam-container'>
             <div className={
@@ -21,11 +22,13 @@ function Home() {
             <p>{camera.id}</p>
         </div>
     );
+
     return (
             <div className='home'>
                 <h1>Cameras</h1>
                 <div className='stopper'></div>
-                <div className='all-cameras'>{listCameras}</div>
+                <div className='all-cameras'>{listCameras} {(cameras.length === 0) ? "No camera!" : null}</div>
+                {fetch_cam}
             </div>
 
     );
