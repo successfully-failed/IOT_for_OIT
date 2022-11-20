818a8de7e9dda7f34c8c4cab306c6c7307ec9b9d
successfully-failed <bronislawwroblewski05@gmail.com>
2022-11-20 09:24:46 +0100

    added a usefull module

---
+++ limiter.py
@@ -0,0 +1,15 @@
+import resource
+
+
+def poor_mode (seconds, maxsize):
+    soft, hard = resource.getrlimit(resource.RLIMIT_CPU)
+    softm, hardm = resource.getrlimit(resource.RLIMIT_AS)
+    print("lowering resources")
+    resource.setrlimit(resource.RLIMIT_CPU, (seconds, hard))
+    resource.setrlimit(resource.RLIMIT_AS, (maxsize, hardm))
+    print("resources taken way")
+
+def rich_mode (seconds, maxsize):
+    print("lowering resources")
+    resource.RLIM_INFINITY 
+    print("resources taken way")
